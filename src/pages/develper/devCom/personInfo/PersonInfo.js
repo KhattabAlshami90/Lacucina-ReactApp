edf77d2baf10fd9f5ea31da82df416b81255a99e
Khattab Alshami <khmoal90@gmail.com>
2023-05-13 11:18:27 +0200

    Change personInfo

--- src/pages/develper/devCom/personInfo/PersonInfo.js
+++ src/pages/develper/devCom/personInfo/PersonInfo.js
@@ -20,7 +20,7 @@ const PersonInfo = () => {
         <RubberBand>
           <div className="info-text" id="info-text">
             <h2 className="info-h">Khattab Alshami</h2>
-            <h2 className="info-h">khmoal90@gamil.com</h2>
+            <h2 className="info-h">khmoal90@gmail.com</h2>
             <h2 className="info-h">076-327 20 92</h2>
           </div>
         </RubberBand>
